b44f4f5433f78b245fb9fee2fe56f569980f8f9c
Takuto Yanagida <tokutadaginaya@gmail.com>
2026-06-27 21:57:44 +0900

    Improve access control

--- src/v1/index.php
+++ src/v1/index.php
@@ -4,37 +4,69 @@
  * Weather API
  *
  * @author Takuto Yanagida
- * @version 2026-04-24
+ * @version 2026-06-27
  *
  */
 
+require "access-control.php";
+
+$allowed_hosts = [
+	'takty.net',
+];
+
+$expected_uas = [
+	'Croqujs/',
+	'Electron/',
+];
+
 const OWNER = 'takty';
+
+$is_allowed_hosts = is_request_allowed($allowed_hosts);
+
+if (!$is_allowed_hosts && !is_user_agent_expected($expected_uas)) {
+	http_response_code(404);
+	return;
+}
+if ($is_allowed_hosts) {
+	send_cors_headers();
+}
+
+
+// -----------------------------------------------------------------------------
+
+
+$api_key = getenv('API_KEY');
 
 $lat = 0;
 $lot = 0;
 
 if (
-	isset( $_GET['lat'] ) && ! preg_match( '/[^0-9]/', $_GET['lat'] ) &&
-	isset( $_GET['lon'] ) && ! preg_match( '/[^0-9]/', $_GET['lon'] )
+	isset($_GET['lat']) && !preg_match('/[^0-9\.]/', $_GET['lat']) &&
+	isset($_GET['lon']) && !preg_match('/[^0-9\.]/', $_GET['lon'])
 ) {
-	$lat = round( floatval( $_GET['lat'] ) );
-	$lon = round( floatval( $_GET['lon'] ) );
+	$lat = round(floatval($_GET['lat']));
+	$lon = round(floatval($_GET['lon']));
 } else {
-	header( 'Content-Type: text/html; charset=UTF-8' );
-	echo json_encode( [ 'status' => 'no' ] );
+	http_response_code(403);
+	header('Content-Type: application/json; charset=UTF-8');
+	echo json_encode(['status' => 'no']);
 	return;
 }
 
 clean_cache();
-$w = read_cache( $lat, $lon );
-if ( $w === null ) {
-	$w = get_weather( $lat, $lon );
-	if ( $w ) write_cache( $lat, $lon, $w );
+$w = read_cache($lat, $lon);
+if ($w === null) {
+	$w = get_weather($api_key, $lat, $lon);
+	if ($w !== null) {
+		write_cache($lat, $lon, $w);
+	}
 }
-if ( ! $w ) $w = [ 'status' => 'no' ];
 
-header( 'Content-Type: text/html; charset=UTF-8' );
-echo json_encode( $w );
+if ($w === null) {
+	http_response_code(502);
+}
+header('Content-Type: application/json; charset=UTF-8');
+echo json_encode($w);
 
 
 // -----------------------------------------------------------------------------
@@ -42,71 +74,74 @@ echo json_encode( $w );
 
 function clean_cache() {
 	$dir = __DIR__ . '/cache/';
-	if ( ! file_exists( $dir ) ) return;
+	if (!file_exists($dir)) return;
 
-	$now = new DateTime( 'now' );
-	$ps = scandir( $dir );
-	foreach ( $ps as $p ) {
-		if ( $p[0] === '.' ) continue;
-		$ps = explode( ',', $p );
-		if ( count( $ps ) === 3 ) {
-			$d = preg_replace( '/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/', '${1}-${2}-${3} ${4}:${5}:00', $ps[2] );
-			$date = new DateTime( $d );
-			$diff = $date->diff( $now );
-			if ( $diff->days === 0 && $diff->h === 0 && $diff->i < 30 ) continue;
+	$now = new DateTime('now');
+	$ps  = scandir($dir);
+
+	foreach ($ps as $p) {
+		if ($p[0] === '.') continue;
+		$ps = explode(',', $p);
+
+		if (count($ps) === 3) {
+			$d    = preg_replace('/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})/', '${1}-${2}-${3} ${4}:${5}:00', $ps[2]);
+			$date = new DateTime($d);
+			$diff = $date->diff($now);
+			if ($diff->days === 0 && $diff->h === 0 && $diff->i < 30) continue;
 		}
 		unlink( $dir . $p );
 	}
 }
 
-function read_cache( $lat, $lon ) {
+function read_cache(float $lat, float $lon): ?array {
 	$dir = __DIR__ . '/cache/';
-	if ( ! file_exists( $dir ) ) return null;
+	if (!file_exists($dir)) return null;
 
-	$ps = scandir( $dir, SCANDIR_SORT_DESCENDING );
-	foreach ( $ps as $p ) {
-		if ( $p[0] === '.' ) continue;
-		$ps = explode( ',', $p );
-		if ( count( $ps ) === 3 && $ps[0] == $lat && $ps[1] == $lon ) {
-			$c = file_get_contents( $dir . $p );
-			return json_decode( $c );
+	$ps = scandir($dir, SCANDIR_SORT_DESCENDING);
+	foreach ($ps as $p) {
+		if ($p[0] === '.') continue;
+		$ps = explode(',', $p);
+
+		if (count($ps) === 3 && $ps[0] == $lat && $ps[1] == $lon) {
+			$c = file_get_contents($dir . $p);
+			return json_decode($c, true);
 		}
 	}
 	return null;
 }
 
-function write_cache( $lat, $lon, $w ) {
+function write_cache(float $lat, float $lon, array $w) {
 	$dir = __DIR__ . '/cache/';
-	if ( ! file_exists( $dir ) ) {
-		$s = mkdir( $dir, 0775, true );
-		if ( $s ) {
-			chmod( $dir, 0775 );
-			chown( $dir, OWNER );
+
+	if (!file_exists($dir)) {
+		$s = mkdir($dir, 0775, true);
+		if ($s) {
+			chmod($dir, 0775);
+			chown($dir, OWNER);
 		}
 	}
-	if ( ! file_exists( $dir ) ) return false;
+	if (!file_exists($dir)) return false;
 
-	$now = new DateTime( 'now' );
-	$ns = $now->format( 'YmdHi' );
-	$fn = "$lat,$lon,$ns";
+	$now  = new DateTime('now');
+	$ns   = $now->format('YmdHi');
+	$fn   = "$lat,$lon,$ns";
 	$path = $dir . '/' . $fn;
-	file_put_contents( $path, json_encode( $w ), LOCK_EX );
-	chown( $path, OWNER );
+	file_put_contents($path, json_encode($w), LOCK_EX);
+	chown($path, OWNER);
 }
 
 
 // -----------------------------------------------------------------------------
 
 
-function get_weather( $lat, $lon ) {
+function get_weather(string $api_key, float $lat, float $lon): ?array {
 	$unit = 'metric';
-	$key  = '20085c5c99ae270b88e30de6720c97b7';
-	$url  = "http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=$unit&appid=$key";
+	$url  = "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=$unit&appid=$api_key";
+	$cont = file_get_contents($url);
+	if ($cont === false) return null;
 
-	$cont = file_get_contents( $url );
-	if ( $cont === false ) return null;
-	$raw = json_decode( $cont, true );
-	if ( $raw === null ) return null;
+	$raw = json_decode($cont, true);
+	if ($raw === null) return null;
 
 	$res = [];
 	$res['temp']       = $raw['main']['temp'];      // [C deg]
